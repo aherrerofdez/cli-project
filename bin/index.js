4dc9507d35ef84278b450795bcd00a15c4664113
aherrerofdez <aherrero.mis2016@student.ie.edu>
2021-12-24 00:16:44 +0100

    Added multiple lines of user input

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('Who are you? ', name => {
+    console.log(`Hey there ${name}!`);
+    if(name.toLowerCase() == "ana") {
+        readline.question('Where are you? ', place => {
+            console.log(`WOW! ${place} is so cool!`);
+            readline.close();
+        });
+    }
+  });
