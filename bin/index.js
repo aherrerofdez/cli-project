6d14fe11875ac64ec7b8a7cfb988723e609ac3b2
aherrerofdez <aherrero.mis2016@student.ie.edu>
2021-12-27 12:34:13 +0100

    handling other names input options

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,5 @@
+const { read } = require('fs');
+
 console.log("Hello World");
 
 const readline = require('readline').createInterface({
@@ -6,11 +8,14 @@ const readline = require('readline').createInterface({
   });
   
   readline.question('Who are you? ', name => {
-    console.log(`Hey there ${name}!`);
+    console.log(`Hey there ${name} :)`);
     if(name.toLowerCase() == "ana") {
         readline.question('Where are you? ', place => {
             console.log(`WOW! ${place} is so cool!`);
             readline.close();
         });
+    }
+    else {
+      readline.close();
     }
   });
